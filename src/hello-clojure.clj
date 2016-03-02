b42ddb5826be0d389f7b556d40a832d3a6e14219
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-01 20:16:52 -0500

    Initial commit

---
+++ src/hello-clojure.clj
@@ -0,0 +1,9 @@
+; a line comment
+(comment
+  this is a multiline comment
+  no need for the the semicolon)
+
+(defn hello [name]
+  (println "Hello, " name))
+
+(hello "Clojure")
---
+++ src/pig-latin.clj
@@ -0,0 +1,13 @@
+; returns arg if in set, nil otherwise
+(def vowel? (set "aeiou"))
+
+(defn pig-latin [word]
+  (let [first-letter (first word)]
+    (if (vowel? first-letter)
+      (str word "ay")
+      (str (subs word 1) first-letter "ay"))))
+
+(println (pig-latin "red"))       ;edray
+(println (pig-latin "orange"))    ;orangeay
+(println (pig-latin "phrancis"))  ;hrancispay
+(println (pig-latin "sirpython")) ;irpythonsay
