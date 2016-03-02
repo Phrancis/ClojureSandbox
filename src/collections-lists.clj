48cd79cb214da122d852d18ef1a2fdf6c99a800b
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-01 22:04:59 -0500

    finish collections-lists

--- src/collections-lists.clj
+++ src/collections-lists.clj
@@ -4,8 +4,8 @@
 
 ; Here are some ways to create a list that all have the same result:
 (def nums (list "one" "two" "three" "four"))
-(def nums (quote ("one" "two" "three" "four")))
-(def nums '("one" "two" "three" "four"))
+(def nums2 (quote ("one" "two" "three" "four")))
+(def nums3 '("one" "two" "three" "four"))
 
 ; The `some` function can be used to determine if a collection contains a given item.
 ; It takes a predicate function and a collection.
@@ -13,3 +13,19 @@
 ; it is somewhat intentional to discourage this usage.
 ; Searching a list for a single item is a linear operation.
 ; Using a set instead of a list is more efficient and easier.
+(some #(= % "two") nums)    ; true
+(some #(= % "hello") nums)  ; nil
+; Another approach is to create a set from the list
+; and then use the contains? function on the set as follows.
+(contains? (set nums) "one") ; -> true
+
+; While the `conj` function will create a new list, the `cons` function will create a new sequence.
+; In each case the new item(s) are added to the front.
+; The remove function creates a new list containing only the items for which a predicate function returns false.
+(def more-nums (conj nums "zero"))          ; (zero one two three four)
+(def less-nums (remove #(= % "four") nums)) ; (one two three)
+
+; The `into` function creates a new list that contains all the items in two lists.
+(def charz '(\A \B \C))
+(def nums-charz (into nums charz)) ; (C B A one two three four)
+
