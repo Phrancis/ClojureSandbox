8eb11d66b295ceb7c71c6069dfe153672f77792a
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-02 22:11:34 -0500

    update add-test

--- src/clojure1_overview/add-test.clj
+++ src/clojure1_overview/add-test.clj
@@ -5,18 +5,30 @@
 (defn add [x y]
   (+ x y))
 
-
-(println (is (= 4 (add 2 2))))
+; Individual test expressions:
+(is (= 4 (add 2 2)))
   ;true
-(println (is (= 5 (add 2 2))))
+(is (= 5 (add 2 2)))
+  ;FAIL in clojure.lang.PersistentList$EmptyList@1 (add-test.clj:11)
   ;expected: (= 5 (add 2 2))
-  ;actual: (not (= 5 4))
+  ;  actual: (not (= 5 4))
+
+; Expression reports FAIL for any/every failed assertion, however the (are) form is evaluated as a whole
+; and returns true or false apparently based solely on the last statement to be evaluated...
+
+;Evaluates to FALSE:
+(are [x y] (= x y)
+  (add 3 4) 7
+  (add 2 2) 4
+  (add 3 4) 8)
+  ;expected: (= (add 3 4) 8)
+  ;  actual: (not (= 7 8))
   ;false
-(println (are [x y] (= x y)
-           (add 3 4) 7
-           (add 2 2) 4
-           (add 3 4) 8))
-; Returns false for the whole function
-;expected: (= (add 3 4) 8)
-;actual: (not (= 7 8))
-;false
+;Evaluates to TRUE:
+(are [x y] (= x y)
+  (add 3 4) 7
+  (add 3 4) 8
+  (add 2 2) 4)
+  ;expected: (= (add 3 4) 8)
+  ;  actual: (not (= 7 8))
+  ;true
