17cbcc64864b3650a275ada901ec26b0f6701fc3
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-03 08:02:35 -0500

    add namespace to each clj file

--- src/clojure1_overview/java-interoperability.clj
+++ src/clojure1_overview/java-interoperability.clj
@@ -1,2 +1,14 @@
 (ns
   clojure1_overview.java-interoperability)
+
+; Clojure programs can use all Java classes and interfaces. As in Java, classes in the `java.lang` package can be used without
+; importing them. Java classes in other packages can be used by either specifying their package when referencing them
+; or using the import function. For example:
+(import
+  '(java.util
+     Calendar
+     GregorianCalendar)
+  '(javax.swing
+     JFrame
+     JLabel))
+; Also see the `:import` directive of the `ns` macro which is described later.
