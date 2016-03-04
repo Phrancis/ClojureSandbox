46172840137cf1ac8a447cf8afb3c1894099a9ff
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-04 07:21:07 -0500

    add threads.clj

---
+++ src/clojure1_overview/threads.clj
@@ -0,0 +1,18 @@
+(ns
+  clojure1_overview.threads)
+
+; All Clojure functions implement both the java.lang.Runnable interface and the java.util.concurrent.Callable interface.
+; This makes it easy to execute them in new Java threads. For example:
+(defn delayed-print [ms text]
+  (Thread/sleep ms)
+  (println text))
+
+; Pass an anonymous function that invokes delayed-print
+; to the Thread constructor so the delayed-print function
+; executes inside the Thread instead of
+; while the Thread object is being created.
+
+(.start (Thread. #(delayed-print 1000 ", World!"))) ; prints 2nd (1000 ms later)
+(print "Hello") ; prints 1st
+
+; Output: Hello, World!
