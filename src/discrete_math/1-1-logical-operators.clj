3ef45d2511a4e110323f1f118c2efa7763a9aa5b
Phrancis <music@psychoproductions.net>
2016-08-16 18:45:59 -0400

    Improve functions and formatting

--- src/discrete_math/1-1-logical-operators.clj
+++ src/discrete_math/1-1-logical-operators.clj
@@ -1,33 +1,28 @@
-(defn negation [p]
+(defn negation [A]
   "The statement ¬A ('not A') is true if and only if A is false, and false only if A is true"
-  (not p))
+  (not A))
 
 (defn conjunction [A B]
   "The proposition A ∧ B ('A and B') is true if A and B are both true; else it is false."
-  (if
-    (and (true? A) (true? B))
-    true
-    false))
+  (and (true? A)
+       (true? B)))
 
 (defn inclusive-disjunction [A B]
   "The statement A ∨ B ('A or B') is true if A or B (or both) are true; if both are false, the statement is false."
-  (if (or (true? A) (true? B))
-    true
-    false))
+  (or (true? A)
+      (true? B)))
 
 (defn exclusive-disjunction [A B]
   "The statement A ⊕ B ('A or B, but not both') is true when either A or B, but not both, are true."
-  (if (or
-        (and (true? A) (false? B))
-        (and (false? A) (true? B)))
-    true
-    false))
+  (or (and (true?  A)
+           (false? B))
+      (and (false? A)
+           (true?  B))))
 
 (defn implication [A B]
   "A → B ('if A, then B' or 'A implies B') is true only in the case that either A is false or B is true."
-  (if (or (false? A) (true? B))
-    true
-    false))
+  (or (false? A)
+      (true?  B)))
 
 (defn converse [A B]
   "The proposition B → A is the converse of A → B."
@@ -35,55 +30,63 @@
 
 (defn contrapositive [A B]
   "The proposition ¬B → ¬A is the contrapositive of A → B"
-  (implication (negation B) (negation A)))
+  (implication (not B)
+               (not A)))
 
 (defn biconditional [A B]
   "A ↔ B ('A if and only if B') is true only if both A and B are false, or both A and B are true."
-  (if
-    (or (and (true? A) (true? B))
-        (and (false? A) (false? B)))
-    true
-    false))
+  (or (and (true?  A)
+           (true?  B))
+      (and (false? A)
+           (false? B))))
 
-(defn -main []
-  (println "Negation:")
-  (println (negation true))   ; false
-  (println (negation false))  ; true
-  (println "Conjunction:")
-  (println (conjunction true true))   ; true
-  (println (conjunction true false))  ; false
-  (println (conjunction false true))  ; false
-  (println (conjunction false false)) ; false
-  (println "Inclusive disjunction:")
-  (println (inclusive-disjunction true true))   ; true
-  (println (inclusive-disjunction true false))  ; true
-  (println (inclusive-disjunction false true))  ; true
-  (println (inclusive-disjunction false false)) ; false
-  (println "Exclusive disjunction:")
-  (println (exclusive-disjunction true true))   ; false
-  (println (exclusive-disjunction true false))  ; true
-  (println (exclusive-disjunction false true))  ; true
-  (println (exclusive-disjunction false false)) ; false
-  (println "Implication:")
-  (println (implication true true))   ; true
-  (println (implication true false))  ; false
-  (println (implication false true))  ; true
-  (println (implication false false)) ; true
-  (println "Converse:")
-  (println (converse true true))    ; true
-  (println (converse true false))   ; true
-  (println (converse false true))   ; false
-  (println (converse false false))  ; true
-  (println "Contrapositive:")
-  (println (contrapositive true true))    ; true
-  (println (contrapositive true false))   ; false
-  (println (contrapositive false true))   ; true
-  (println (contrapositive false false))  ; true
-  (println "Biconditional:")
-  (println (biconditional true true))   ; true
-  (println (biconditional true false))  ; false
-  (println (biconditional false true))  ; false
-  (println (biconditional false false)) ; true
+(defn -main [& args]
+  (println
+    "Negation:"
+    \newline (negation true)    ; false
+    \newline (negation false))  ; true
+  (println
+    "Conjunction:"
+    \newline (conjunction true true)    ; true
+    \newline (conjunction true false)   ; false
+    \newline (conjunction false true)   ; false
+    \newline (conjunction false false)) ; false
+  (println
+    "Inclusive disjunction:"
+    \newline (inclusive-disjunction true true)    ; true
+    \newline (inclusive-disjunction true false)   ; true
+    \newline (inclusive-disjunction false true)   ; true
+    \newline (inclusive-disjunction false false)) ; false
+  (println
+    "Exclusive disjunction:"
+    \newline (exclusive-disjunction true true)    ; false
+    \newline (exclusive-disjunction true false)   ; true
+    \newline (exclusive-disjunction false true)   ; true
+    \newline (exclusive-disjunction false false)) ; false
+  (println
+    "Implication:"
+    \newline (implication true true)    ; true
+    \newline (implication true false)   ; false
+    \newline (implication false true)   ; true
+    \newline (implication false false)) ; true
+  (println
+    "Converse:"
+    \newline (converse true true)     ; true
+    \newline (converse true false)    ; true
+    \newline (converse false true)    ; false
+    \newline (converse false false))  ; true
+  (println
+    "Contrapositive:"
+    \newline (contrapositive true true)    ; true
+    \newline (contrapositive true false)   ; false
+    \newline (contrapositive false true)   ; true
+    \newline (contrapositive false false)) ; true
+  (println
+    "Biconditional:"
+    \newline (biconditional true true)    ; true
+    \newline (biconditional true false)   ; false
+    \newline (biconditional false true)   ; false
+    \newline (biconditional false false)) ; true
   )
 
 (-main)
