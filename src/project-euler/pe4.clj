3db112b9b84214cbbfa71e24a0ff88c0ff1e748c
Phrancis <music@psychoproductions.net>
2016-08-17 15:44:48 -0400

    Solve ProjectEuler4

---
+++ src/project-euler/pe4.clj
@@ -0,0 +1,38 @@
+; ProjectEuler4: Largest palindrome product
+; A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+; Find the largest palindrome made from the product of two 3-digit numbers.
+
+(defn palindrome? [arg]
+  "Returns true if arg is a palindrome, i.e., it reads the same both ways"
+  (= (seq (str arg))
+     (reverse (str arg))))
+
+(defn pow [N X]
+  "Returns N to the power of X"
+  {:pre [(number? N) (number? X)]}
+  (reduce * (repeat X N)))
+
+(defn nums-with-n-digits [digits]
+  "Return list of all numbers comprised of a certain number of digits"
+  {:pre (number? digits)}
+  (let [max (pow 10 digits)
+        min (/ max 10)]
+    (range min max)))
+
+(defn products-of-nums-list [nums]
+  "Returns a sorted list of distinct products of each of the numbers in nums"
+  {:pre (list? nums)}
+  (sort < (distinct (for [x nums y nums] (* x y)))))
+
+(defn solve-pe4 [digits]
+  "Returns largest palindrome product of all numbers with a certain number of digits"
+  {:pre (number? digits)}
+  (apply max (filter palindrome? (products-of-nums-list (nums-with-n-digits digits)))))
+
+
+(defn -main []
+  (println "ProjectEuler4: Largest palindrome product")
+  (println (solve-pe4 2))
+  (println (solve-pe4 3)))
+(-main)
+
