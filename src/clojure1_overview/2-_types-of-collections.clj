58114f0a4292c2bbb531c3b59304807e077c046f
Phrancis <music@psychoproductions.net>
2016-08-17 12:10:18 -0400

    Improve clojure1_overview.2--types-of-collections with println

--- src/clojure1_overview/2-_types-of-collections.clj
+++ src/clojure1_overview/2-_types-of-collections.clj
@@ -4,29 +4,34 @@
 ; Clojure has 4 core types of collections,
 
 ; VECTORS
-; Vectors are ordered collecions of items.
+; Vectors are ordered collections of items.
 ; - Best for adding/removing items from the back
 ; - Efficient for finding or changing items by index
 ; - Function and macro arguments are generally in the form of a vector
 (def vector1 (vector "one" "two" "three" "four"))
-(def vector2 ["one" "two" "three" "four"])
+(def vector2 ["one" "three" "two" "four"])
 
 ; LISTS
-; Lists are unordered colletions of items.
+; Lists are unordered collections of items.
 ; - Best for adding/removing items from the front
 ; - Inefficient for finding or changing items by index
 (def list1 (list "one" "two" "three" "four"))
-(def list2 (quote ("one" "two" "three" "four")))
-(def list3 '("one" "two" "three" "four"))
+(def list2 (quote ("one" "three" "two" "four")))
+;; Note the single-quote syntax:
+(def list3 '("four" "one" "two" "three"))
+
 
 ; SETS
 ; Sets are collections of unique items.
 ; - Clojure supports two kinds of sets, unsorted and sorted
 ; - Preferred over lists and vectors when duplicates are not allowed and items do not need
 ; to be maintained in the order in which they were added
-(def set1 (hash-set 1 4 2 3))
-(def set2 #{1 4 2 3})
+(def unsorted-set1 (hash-set 1 4 2 3))
+(def unsorted-set2 #{1 4 2 3})
+;; THIS WILL CAUSE AN ERROR DUE TO DUPLICATE KEY:
+;; (def unsorted-set3 (hash-set 1 1 4 4 2 2 3 3))
 (def sorted-set1 (sorted-set 1 4 2 3))
+
 
 ; MAPS
 ; Maps store associations between keys and their corresponding values where both can be any kind of object.
@@ -35,4 +40,18 @@
 ; - Commas are treated as white space but can help readability
 (def map1 (hash-map :DC "Washington" :AZ "Phoenix" :CA "Sacramento"))
 (def map2 {:DC "Washington" :AZ "Phoenix" :CA "Sacramento"})
-(def sorted-map1 (sorted-map 1 "one" 2 "two" 3 "three"))
+(def sorted-map1 (sorted-map 3 "three" 1 "one" 2 "two"))
+
+(defn -main []
+  (println "vector1" vector1)
+  (println "vector2" vector2)
+  (println "list1" list1)
+  (println "list2" list2)
+  (println "list3" list3)
+  (println "unsorted-set1" unsorted-set1)
+  (println "unsorted-set2" unsorted-set2)
+  (println "sorted-set1" sorted-set1)
+  (println "map1" map1)
+  (println "map2" map2)
+  (println "sorted-map1" sorted-map1))
+(-main)
