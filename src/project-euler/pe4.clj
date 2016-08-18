06f011b21c78b42a1e45361b9b0ab8b5475a4930
Phrancis <music@psychoproductions.net>
2016-08-18 15:18:39 -0400

    Made improvements and added test after posting on Code Review

--- src/project-euler/pe4.clj
+++ src/project-euler/pe4.clj
@@ -1,37 +1,110 @@
+(ns project-euler.pe4
+    (require [clojure.string :as string]
+             [clojure.test :as test]))
+
 ; ProjectEuler4: Largest palindrome product
 ; A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 ; Find the largest palindrome made from the product of two 3-digit numbers.
 
-(defn palindrome? [arg]
-  "Returns true if arg is a palindrome, i.e., it reads the same both ways"
-  (= (seq (str arg))
-     (reverse (str arg))))
+(defn palindrome?
+  "Returns true if x reads the same both ways.
+  Returns false for `blank` arguments: nil, empty string or only white space."
+  [x]
+  (let [s (str x)]
+       (cond
+         (nil? x) false
+         :else (= s (string/reverse s)))))
 
-(defn pow [N X]
-  "Returns N to the power of X"
-  {:pre [(number? N) (number? X)]}
-  (reduce * (repeat X N)))
+(test/deftest palindrome?-test
+  ; numbers
+  (test/is (true? (palindrome? 303)))
+  (test/is (false? (palindrome? 1234)))
+  (test/is (true? (palindrome? 1)))
+  ; strings
+  (test/is (true? (palindrome? "ABBA")))
+  (test/is (true? (palindrome? "XOX")))
+  (test/is (false? (palindrome? "ABC")))
+  (test/is (true? (palindrome? "")))
+  (test/is (true? (palindrome? "    "))) ; even number of spaces
+  (test/is (true? (palindrome? "   ")))  ; odd number of spaces
+  ; others
+  (test/is (false? (palindrome? nil))))
 
-(defn nums-with-n-digits [digits]
-  "Return list of all numbers comprised of a certain number of digits"
-  {:pre (number? digits)}
-  (let [max (pow 10 digits)
-        min (/ max 10)]
-    (range min max)))
+(defn make-pos
+  "Makes n into a positive number if it is not already."
+  [n]
+  {:pre [(number? n)]}
+  (if (neg? n) (- n) n))
 
-(defn products-of-nums-list [nums]
-  "Returns a descending order list of distinct products of each of the numbers in nums"
-  {:pre (list? nums)}
-  (sort > (distinct (for [x nums y nums] (* x y)))))
+(test/deftest make-pos-test
+  (test/is (pos? (make-pos 42)))
+  (test/is (pos? (make-pos -42)))
+  (test/is (zero? (make-pos 0))))
 
-(defn solve-pe4 [digits]
-  "Returns list of solutions of largest palindrome products of all numbers with a certain number of digits"
-  {:pre (number? digits)}
-  (first (filter palindrome? (products-of-nums-list (nums-with-n-digits digits)))))
+(defn pow
+  "Returns x to the power of y"
+  [x y]
+  {:pre [(number? x)
+         (number? y)]}
+  (let [result (reduce * (repeat (make-pos y) (make-pos x)))]
+    (cond
+      (zero? y) 1
+      (and (neg? x) (neg? y)) (cond
+                                (odd? y) (- (/ 1 result))
+                                :else (/ 1 result))
+      (neg? y) (/ 1 result)
+      (neg? x) (- result)
+      :else result)))
+
+(test/deftest pow-test
+  (test/is (= 4 (pow 2 2)))
+  (test/is (= 100 (pow 10 2)))
+  (test/is (= 1 (pow 2 0)))
+  (test/is (= 1/4 (pow 2 -2)))
+  (test/is (= -4 (pow -2 2)))
+  (test/is (= -8 (pow -2 3)))
+  (test/is (= 1/4 (pow -2 -2)))
+  (test/is (= -1/8 (pow -2 -3))))
+
+(defn nums-with-n-digits
+  "Return a lazy sequence of all positive integers with n digits."
+  [n]
+  {:pre [(number? n)]}
+  (let [end (pow 10 n)
+        start (/ end 10)]
+    (range start end)))
+
+(test/deftest nums-with-n-digits-test
+  (test/is (= 9 (count (nums-with-n-digits 1))))
+  (test/is (= 1 (apply min (nums-with-n-digits 1))))
+  (test/is (= 9 (apply max (nums-with-n-digits 1))))
+  (test/is (= 90 (count (nums-with-n-digits 2))))
+  (test/is (= 10 (apply min (nums-with-n-digits 2))))
+  (test/is (= 99 (apply max (nums-with-n-digits 2)))))
+
+(defn solve-pe4
+  "Returns the largest palindrome made from the product of two n-digit numbers."
+  [n]
+  {:pre [(number? n)]}
+  (->> (let [nums (nums-with-n-digits n)]
+         ; `for` handles multiplying each of the numbers with each other
+         ; to get the products of n-digit numbers.
+         (for [x nums
+               y nums]
+           (* x y)))
+    (filter palindrome?)
+    (apply max)))
+
+(test/deftest solve-pe4-test
+  (test/is (= 9009 (solve-pe4 2)))
+  (test/is (= 906609 (solve-pe4 3))))
 
 (defn -main []
-  (println (solve-pe4 2)) ; 9009
-  (println (solve-pe4 3))); correct answer
+  (test/run-tests)
+  (println (solve-pe4 2))   ; 9009
+  (println (solve-pe4 3))   ; 906609
+  ; (println (solve-pe4 4))  ; 99000099
+  )
 
 (-main)
 
